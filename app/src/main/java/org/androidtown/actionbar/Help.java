c2203b02b29ecefb4a5f24266673175521be6cba
J Bin <J Bin>
2016-11-02 17:20:08 +0900

    움직임 알람 주기 뺌

---
+++ app/src/main/java/org/androidtown/actionbar/Help.java
@@ -0,0 +1,13 @@
+package org.androidtown.actionbar;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class Help extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_layout);
+    }
+}
